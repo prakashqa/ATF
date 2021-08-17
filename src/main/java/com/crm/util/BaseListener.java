1e457aa0ad4cce2617a4382f3bf5e0870e3cd180
Prakash <prakash.hssk@gmail.com>
2021-08-17 19:45:41 +0530

    removed screen shots

--- src/main/java/com/crm/util/BaseListener.java
+++ src/main/java/com/crm/util/BaseListener.java
@@ -13,37 +13,28 @@ import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.crm.config.BrowserDriver;
 
 public class BaseListener extends TestListenerAdapter {
-	
-	static ExtentTest elogger;
-	static ExtentReports extent;
+
 	
 	@Override
 	public void onTestStart(ITestResult itr) {
-		extent = new ExtentReports();
 		log("Test Started..........");
-		//elogger.info("Test Started.....");
 	}
 	
 	@Override
 	public void onTestSuccess(ITestResult itr) {
-		extent = new ExtentReports();
 		log("Test " +itr.getName() + " Passed");
 		log(itr.getTestClass());
 		log("Priority of the method is " +itr.getMethod().getPriority());
-		//elogger.pass(itr.getThrowable());
-		//elogger.log(Status.PASS, MarkupHelper.createLabel(itr.getName() + " Test Case Passed ", ExtentColor.GREEN));
+
 	}
 	
 	@Override
 	public void onTestFailure(ITestResult itr) {
-		extent = new ExtentReports();
 		BrowserDriver.takeScreenShot();
 		log("Test " +itr.getName() + " Failed");
 		log(itr.getTestClass());
 		log("Priority of the method is " +itr.getMethod().getPriority());
-		
-		//elogger.fail(itr.getThrowable());
-		//elogger.log(Status.PASS, MarkupHelper.createLabel(itr.getName() + " Test Case Failed ", ExtentColor.RED));
+
 	}
 	
 	private void log(String methodName) {
